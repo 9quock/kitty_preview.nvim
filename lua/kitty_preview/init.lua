ed024b02b63616a7abea4483b8a4d1fa1f95e613
9quock <lolistme@proton.me>
2024-11-05 22:24:44 +0300

    Initial commit

---
+++ lua/kitty_preview/init.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+local function GetFileExtension(url)
+    return url:match("^.+(%..+)$")
+end
+
+function M.IsImage(url)
+    local extension = GetFileExtension(url)
+
+    if extension == '.bmp' then
+        return true
+    elseif extension == '.jpg' or extension == '.jpeg' then
+        return true
+    elseif extension == '.png' then
+        return true
+    elseif extension == '.gif' then
+        return true
+    end
+
+    return false
+end
+
+function M.PreviewImage(absolutePath)
+    if M.IsImage(absolutePath) then
+        vim.api.nvim_command('kitty @ launch --type=window kitty icat --hold "' .. absolutePath .. '"')
+    else
+        print("No preview for file " .. absolutePath)
+    end
+end
+
+function M.PreviewImageNvimTree()
+  local kitty_preview = require("kitty_preview")
+  local nvimtree = require("nvim-tree.api")
+  local path = nvimtree.tree.get_node_under_cursor().absolutePath
+  kitty_preview.PreviewImage(path)
+end
+
+function M.setup()
+  vim.keymap.set("n", "<leader>P", function() require("kitty_preview").PreviewImageNvimTree() end, { desc = "Preview image under cursor" })
+end
+
+return M
